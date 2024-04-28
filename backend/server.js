55c3d984eac31db1424cf87af06a83f409a7f188
Clue <108814268+C1u3@users.noreply.github.com>
2024-04-28 08:57:04 +0000

    Updated Backend

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,7 @@ import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 
 import connectToMongoDB from "./db/connectToMongoDB.js"
-
-const app = express()
+import { app, server } from "./socket/socket.js"
 
 dotenv.config()
 
@@ -21,7 +20,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB()
     console.log(`Server running on port ${PORT}`)
 })
